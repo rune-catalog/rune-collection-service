7f24b25da994869971f5c56b9d65504e8ae68c68
Sam Noedel <sam.noedel@gmail.com>
2016-10-22 09:56:12 -0700

    Add PATCH endpoint

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 const restify = require('restify'),
   slug = require('slug'),
-  MongoClient = require('mongodb').MongoClient
+  MongoClient = require('mongodb').MongoClient,
+  R = require('ramda')
 
 let server = restify.createServer()
 
-server.use(restify.bodyParser())
+server.use(restify.bodyParser({
+  mapParams: false
+}))
 
 server.get('/collection/:user/:collection', (req, res, next) => {
   let db
@@ -50,6 +53,81 @@ server.post('/collection/:user', (req, res, next) => {
       db = database
       let collection = db.collection('collections')
       return collection.insertOne(model);
+    })
+    .catch(err => next(err))
+    .then(() => {
+      db.close()
+      next()
+    })
+})
+
+server.patch('/collection/:user/:collection', (req, res, next) => {
+  let db, collection
+
+  MongoClient.connect('mongodb://localhost:27017/rune')
+    .then(database => {
+      db = database
+      collection = db.collection('collections')
+    })
+    .then(() => {
+      let existing = collection.findOne({
+        user: req.params.user,
+        slug: req.params.collection
+      }, {
+        _id: 0,
+        cards: 1
+      }).then(doc => doc.cards)
+
+      let updated = R.map(card => {
+        let model = {
+          name: card.name,
+          quantity: card.quantity
+        }
+        if (card.set) model.set = card.set
+        return model
+      }, req.body)
+
+      return Promise.all([
+        existing,
+        updated
+      ])
+    })
+    .then(state => {
+      let [ existing, updated ] = state,
+        inserts = [ ],
+        deletes = [ ],
+        updates = [ ]
+
+      R.forEach(card => {
+        if (card.quantity <= 0) {
+          deletes.push(card)
+        }
+        else if (!R.any((a, b) => a.name === b.name), existing) {
+          inserts.push(card)
+        }
+        else {
+          updates.push(card)
+        }
+      }, updated)
+
+      return Promise.all([
+        Promise.resolve(inserts),
+        Promise.resolve(updates),
+        Promise.resolve(deletes)
+      ])
+    })
+    .then(state => {
+      let [ inserts, updates, deletes ] = state
+
+      let insertPromise = collection.insertMany(inserts)
+      let updatePromise = collection.updateMany(updates)
+      let deletePromise = collection.deleteMany()
+
+      return Promise.all([
+        insert,
+        update,
+        del
+      ])
     })
     .catch(err => next(err))
     .then(() => {
